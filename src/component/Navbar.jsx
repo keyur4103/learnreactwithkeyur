9b2a54b9de20570825bc4509f7e986d48a4ef29b
keyur4103 <113822409+keyur4103@users.noreply.github.com>
2024-03-11 18:31:56 +0530

    react routers

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -108,7 +108,7 @@ function Navbar() {
               href="/reactrouter"
               className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
             >
-              React Router"
+              React Router
             </a>
             <a
               href="#"
