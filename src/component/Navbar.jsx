63b84c97d7542fd5d1b5c79cfed85fa2fa194c20
keyur4103 <113822409+keyur4103@users.noreply.github.com>
2024-10-01 20:00:33 +0530

    issue routing

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,11 +1,29 @@
 import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isHidden, setIsHidden] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      const isScrolledDown = currentScrollPos > 0;
+      setIsHidden(isScrolledDown);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <>
-      <nav className="bg-gray-800 sticky w-full top-0 z-10">
+      <nav
+        // className="bg-gray-800 sticky w-full top-0 z-10"
+        className={`bg-gray-800 sticky w-full top-0 z-10 ${
+          isHidden ? "-translate-y-full" : ""
+        } transition-transform duration-300`}
+      >
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
@@ -13,24 +31,24 @@ function Navbar() {
             </div>
             <div className="hidden md:block">
               <div className="flex ml-10 space-x-4">
-                <a
-                  href="/"
+                <Link
+                  to="/"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Introduction
-                </a>
-                <a
-                  href="/hooks"
+                </Link>
+                <Link
+                  to="/hooks"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   React Hooks
-                </a>
-                <a
-                  href="/reactrouter"
+                </Link>
+                <Link
+                  to="/reactrouter"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   React Router
-                </a>
+                </Link>
                 <a
                   href="#"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
@@ -92,24 +110,24 @@ function Navbar() {
           id="mobile-menu"
         >
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            <a
-              href="/"
+            <Link
+              to="/"
               className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
             >
               Introduction
-            </a>
-            <a
-              href="/hooks"
+            </Link>
+            <Link
+              to="/hooks"
               className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
             >
               React Hooks
-            </a>
-            <a
-              href="/reactrouter"
+            </Link>
+            <Link
+              to="/reactrouter"
               className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
             >
               React Router
-            </a>
+            </Link>
             <a
               href="#"
               className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
